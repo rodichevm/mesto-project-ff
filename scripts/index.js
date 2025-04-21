3a62751a14ff601db8a468c04b9c41c37362c3a2
Mikhail Rodichev <rodichevmichael@gmail.com>
2025-04-22 00:42:18 +0300

    Refactor HTML structure and improve formatting in index.html; update initialCards in cards.js; enhance createCard function in index.js for better readability

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,23 @@ const cardElement = cardTemplate.querySelector('.card');
 const placesList = document.querySelector('.places__list');
 
 function createCard({ name, link, imageDescription }) {
-    const card = cardElement.cloneNode(true);
-    const image = card.querySelector('.card__image');
-    image.src = link;
-    image.alt = imageDescription;
-    card.querySelector('.card__title').textContent = name;
-    const deleteButton = card.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => removeCard(card));
-    return card;
+  const card = cardElement.cloneNode(true);
+  const image = card.querySelector('.card__image');
+  image.src = link;
+  image.alt = imageDescription;
+  card.querySelector('.card__title').textContent = name;
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => removeCard(card));
+  return card;
 }
 
 function removeCard(card) {
-    card.remove();
+  card.remove();
 }
 
 const currentYearElement = document.querySelector('#current-year');
 if (currentYearElement) {
-    currentYearElement.textContent = new Date().getFullYear().toString();
+  currentYearElement.textContent = new Date().getFullYear().toString();
 }
 
 initialCards.forEach((card) => placesList.append(createCard(card)));
