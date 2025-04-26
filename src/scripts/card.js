0d8830ecbd6ede0057bd45e80b9fdd2edf7dc9d2
Mikhail Rodichev <rodichevmichael@gmail.com>
2025-04-26 17:23:00 +0300

    feat: update index.html structure and add card creation functionality

---
+++ src/scripts/card.js
@@ -0,0 +1,17 @@
+function createCard({ name, link, imageDescription }, cardTemplate) {
+  const cardElement = cardTemplate.querySelector('.card');
+  const card = cardElement.cloneNode(true);
+  const image = card.querySelector('.card__image');
+  image.src = link;
+  image.alt = imageDescription;
+  card.querySelector('.card__title').textContent = name;
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => removeCard(card));
+  return card;
+}
+
+function removeCard(card) {
+  card.remove();
+}
+
+export { createCard };
