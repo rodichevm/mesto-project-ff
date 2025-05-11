e802141f9517955c59a0cffd8e7f58f4e9be184e
Mikhail Rodichev <rodichevmichael@gmail.com>
2025-05-11 17:32:56 +0300

    Add asset filename generation for image and font files in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/[hash][ext][query]',
+        },
       },
       {
         test: /\.css$/,
